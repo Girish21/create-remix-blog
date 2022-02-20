5878803e802f6c4e6c53ec2c7a1287123d2a23b7
Girish21 <girishkumarv21@gmail.com>
2022-02-20 17:48:19 +0530

    🔥 feat: cli to generate project from templates

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,20 @@
 import * as inquirer from 'inquirer'
+import nodepath from 'path'
+import meow from 'meow'
 
-import { buildApp } from '.'
+import type { DB, Language, Servers } from '.'
+import { createApp } from '.'
+
+const Banner = `
+  Usage:
+    $ npx create-remix-blog [flags...] [<dir>]
+
+  if <dir> is not provided upfront you will be promted later
+
+  Flags:
+    --help, -h        Show this help message
+    --version, -v     Show the version of this script
+`
 
 run()
   .then(() => {
@@ -12,13 +26,66 @@ run()
   })
 
 async function run() {
+  const { input } = meow(Banner, {
+    flags: {
+      help: { default: false, type: 'boolean', alias: 'h' },
+      version: { default: false, type: 'boolean', alias: 'v' },
+    },
+  })
+
   console.log('\n💿 Remix Blog\n')
 
-  const { name } = await inquirer.prompt<{ name: string }>([
-    { message: 'Hello', name: 'name' },
+  const projectDir = nodepath.resolve(
+    process.cwd(),
+    input.length > 0
+      ? input[0]
+      : (
+          await inquirer.prompt<{ name: string }>({
+            message: 'Where would you like to create your Remix Blog?',
+            default: './my-personal-blog',
+            type: 'input',
+            name: 'name',
+          })
+        ).name
+  )
+
+  const answers = await inquirer.prompt<{
+    server: Servers
+    cacheType: DB
+    lang: Language
+  }>([
+    {
+      message: 'Where do you want to deploy your blog?',
+      type: 'list',
+      choices: [{ name: 'Fly.io (https://fly.io/)', value: 'fly' }],
+      name: 'server',
+    },
+    {
+      message:
+        "How would you want to persist your MDX? Choose SQLite if you're unsure",
+      when(answers) {
+        return answers.server === 'fly'
+      },
+      type: 'list',
+      choices: [
+        { name: 'SQLite', value: 'sqlite' },
+        // { name: 'Postgres (Fly.io)', value: 'fly-pg' },
+        // { name: 'Redis (Fly.io)', value: 'redis' },
+        // { name: 'Postgres', value: 'pg' },
+        { name: 'None', value: 'none' },
+      ],
+      name: 'cacheType',
+    },
+    {
+      message: 'TypeScript or JavaScript',
+      type: 'list',
+      choices: [
+        { name: 'TypeScript', value: 'ts' },
+        { name: 'JavaScript', value: 'js' },
+      ],
+      name: 'lang',
+    },
   ])
 
-  console.log(name)
-
-  buildApp()
+  createApp(projectDir, answers.server, answers.lang, answers.cacheType)
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,115 @@
-export const buildApp = () => {
-  return null
+import fs from 'fs-extra'
+import path from 'path'
+import jsonfs from 'jsonfile'
+import childprocess from 'child_process'
+
+export type Servers = 'fly'
+
+export type Language = 'ts' | 'js'
+
+export type DB = 'sqlite'
+
+export function createApp(
+  projectDir: string,
+  server: Servers,
+  language: Language,
+  db: DB
+) {
+  const versions = process.versions
+
+  if (!versions.node) {
+    throw new Error(
+      'Remix requires Node.js, please setup Node in your environment before continuing with the rest of the setup'
+    )
+  }
+
+  // check Node version
+  if (versions.node && parseInt(versions.node) < 14) {
+    throw new Error(
+      `Remix requires a Node.js version of 14 or greater, but an older version of Node.js was detected ${versions.node}. Please update to Node.js version 14 or greater before continuing`
+    )
+  }
+
+  const relativePath = path.relative(process.cwd(), projectDir)
+  const isCurrentDirectory = relativePath === ''
+
+  // create directory if not exist
+  if (!isCurrentDirectory) {
+    if (fs.existsSync(projectDir)) {
+      console.log(
+        `\n'${relativePath}' already exist. Please try again with another directory\n`
+      )
+      process.exit(1)
+    } else {
+      fs.mkdirSync(relativePath)
+    }
+  }
+
+  // copy from templates to project directory
+  const commonTemplate = path.resolve(
+    process.cwd(),
+    'templates',
+    `common-${language}`
+  )
+  fs.copySync(commonTemplate, projectDir, { overwrite: true })
+
+  const serverTemplate = path.resolve(process.cwd(), 'templates', server)
+  if (fs.existsSync(serverTemplate)) {
+    fs.copySync(serverTemplate, projectDir, {
+      overwrite: true,
+    })
+  }
+
+  const dbTemplate = path.resolve(
+    process.cwd(),
+    'templates',
+    `${server}-${db}-${language}`
+  )
+  if (fs.existsSync(dbTemplate)) {
+    fs.copySync(dbTemplate, projectDir, {
+      overwrite: true,
+    })
+  }
+
+  // transform all dot files (they were removed to play nicely with linters)
+  const dotFiles = [
+    'github',
+    'env.example',
+    'eslintignore',
+    'eslintrc.js',
+    'gitignore',
+    'prettierignore',
+    'prettierrc',
+    'dockerignore',
+  ]
+  for (const file of dotFiles) {
+    const filePath = path.join(projectDir, file)
+    if (fs.existsSync(filePath)) {
+      fs.renameSync(
+        path.join(projectDir, file),
+        path.join(projectDir, `.${file}`)
+      )
+    }
+  }
+
+  // merge packages from templates
+  const appPkg = jsonfs.readFileSync(path.join(commonTemplate, 'package.json'))
+  for (const template of [serverTemplate, dbTemplate]) {
+    if (fs.existsSync(template)) {
+      const templatePkg = jsonfs.readFileSync(
+        path.join(template, 'package.json')
+      )
+      for (const section of ['dependencies', 'devDependencies', 'scripts']) {
+        Object.assign(appPkg[section], templatePkg[section])
+      }
+    }
+  }
+
+  // write back project package
+  jsonfs.writeFileSync(path.join(projectDir, 'package.json'), appPkg, {
+    spaces: 2,
+  })
+
+  // run npm install
+  childprocess.execSync('npm install', { cwd: projectDir, stdio: 'inherit' })
 }
