9f5f5238eb546f26c6c580bb7129a47de1bb34a4
Girish21 <girishkumarv21@gmail.com>
2022-03-05 17:10:50 +0530

    chore: handle request errors

--- src/templates/fly-sqlite-ts/others/utils.js
+++ src/templates/fly-sqlite-ts/others/utils.js
@@ -2,21 +2,28 @@ const execSync = require('child_process').execSync
 
 async function fetchJSON({ http = require('https'), url }) {
   return new Promise((resolve, reject) => {
-    http.get(url, res => {
-      let data = ''
-      res.on('data', chunk => (data += chunk))
-      res
-        .on('end', () => {
-          try {
-            resolve(JSON.parse(data))
-          } catch (e) {
-            reject(e)
-          }
-        })
-        .on('error', error => {
-          reject(error)
-        })
-    })
+    try {
+      const req = http.get(url, res => {
+        let data = ''
+        res.on('data', chunk => (data += chunk))
+        res
+          .on('end', () => {
+            try {
+              resolve(JSON.parse(data))
+            } catch (e) {
+              reject(e)
+            }
+          })
+          .on('error', error => {
+            reject(error)
+          })
+      })
+      req.on('error', error => {
+        reject(error)
+      })
+    } catch (error) {
+      reject(error)
+    }
   })
 }
 
